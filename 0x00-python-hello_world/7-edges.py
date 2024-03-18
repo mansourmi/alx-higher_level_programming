73950806565355f8408a606e2facfa28b48d8a90
mansourmi <mansourmubarak35@gmail.com>
2024-03-18 20:16:09 +0200

    re do

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word = "school"
-print(f"First 3 letters: {word[:3]}")
-print(f"Last 2 letters: {word[-2:]}")
-print(f"Middle word: {word[1:7]}")
+word1 = word[:3]
+word2 = word[-2:]
+word3 = word[1:9]
+print(f"First 3 letters: {word1}")
+print(f"Last 2 letters: {word2}")
+print(f"Middle word: {word3}")
